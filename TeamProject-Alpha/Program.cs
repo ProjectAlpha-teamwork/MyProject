42c93ec465ac2549aa3ef8f0e09264a262a37035
Your Name Here <fatalclaw@hotmail.com>
2016-06-24 18:25:02 +0300

    first update of team project
    General Requirements
    
    Please define and implement the following object-oriented assets in your project:
    
    At least 5 interfaces (with one or more implementations)
    At least 15 classes (implementing the application logic)
    At least 3 abstract classes (with inheritors)
    At least 1 custom exception class (with usage in your code)
    At least 3 levels of depth in inheritance
    At least 1 polymorphism usage
    At least 1 structure
    At least 1 enumeration
    At least 1 event (with subscribers)
    At least 1 design pattern (e.g. Composite, Singleton, Factory, Wrapper, Bridge, Command, etc.)
    You might read about design patterns in Wikipedia, Sourcemaking, DoFactory and others.

---
+++ TeamProject-Alpha/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeamProject_Alpha.Sports;
+
+namespace TeamProject_Alpha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ TeamProject-Alpha/Sports/Sport.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamProject_Alpha.Sports
+{
+    public abstract class Sport
+    {
+
+        
+    }
+}
